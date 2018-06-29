d3c18b619ce3796a76ff5443acbdd06c3d33a39d
Tomonori Yano <yano@eckrel.com>
2018-06-30 05:26:33 +0900

    check same id

--- src/stores/MyListsStore.jsx
+++ src/stores/MyListsStore.jsx
@@ -29,12 +29,12 @@ const MyListsStore = types.model({
     function add(id_or_url, movieListStore) {
         const id = Util.normalizeMylistId(id_or_url);
 
-        console.log(id);
-
-        const mylist = MyListStore.create({id: id});
-        mylist.update(movieListStore);
-        self.lists.set(id, mylist);
-        self.keys.push(id);
+        if(!self.lists.has(id)) {
+            const mylist = MyListStore.create({id: id});
+            mylist.update(movieListStore);
+            self.lists.set(id, mylist);
+            self.keys.push(id);
+        }
     }
 
     function remove(id_or_url) {
