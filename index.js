5e94f5de1825ae0dab48c7eff6c9a1a34429566e
Tomonori Yano <yano@eckrel.com>
2018-07-04 22:48:10 +0900

    Add menu

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const electron = require('electron');
-const {app, BrowserWindow, ipcMain, shell } = electron;
+const {app, BrowserWindow, ipcMain, shell, Menu} = electron;
 const isDev = require('electron-is-dev');
 
 if (isDev) { require('electron-reload')(__dirname) }
@@ -21,6 +21,27 @@ app.on('ready', () => {
             mainWindow.webContents.send('app-close');
         }
     });
+
+    const template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+        ]}, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}
+    ];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
 });
 
